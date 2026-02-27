e2d4139dd55023c77c66ec225bab4bba79b3f563
Mairag0 <mairarodriguesmk@gmail.com>
2026-02-27 10:03:30 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -1,4 +1,4 @@
-"""
+p"""
 Beecrowd 1001 - Extremamente Básico
 
 Leia 2 valores inteiros e armazene-os nas variáveis A e B.
@@ -12,3 +12,12 @@ você receberá "Presentation Error".
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
 
 # Escreva sua solução abaixo
+# Leitura dos valores de entrada
+a = int(input())
+b = int(input())
+
+# Processamento da soma
+x = a + b
+
+# Saída formatada conforme a especificação
+print(f"X = {x}")
